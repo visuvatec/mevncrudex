abf4ba1166291afc1fbd820be5bdb8b0b6c09714
svisuva <svisuva@us.ibm.com>
2019-08-19 01:31:34 -0400

    First Commit

---
+++ src/components/IndexComponent.vue
@@ -0,0 +1,63 @@
+// IndexComponent.vue
+
+<template>
+  <div>
+    <h1>Posts</h1>
+    <div class="row">
+      <div class="col-md-10"></div>
+      <div class="col-md-2">
+        <router-link :to="{ name: 'create' }" class="btn btn-primary">Create Post</router-link>
+      </div>
+    </div>
+    <br />
+
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th>Sl.#</th>
+          <th>Title</th>
+          <th>API Key</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(post, index) in posts" :key="post._id" :row="index">
+          <td>{{index + 1}}</td>
+          <td>{{ post.title }}</td>
+          <td>{{ post.keyuuid }}</td>
+
+          <td>
+            <router-link :to="{name: 'edit', params: { id: post._id }}" class="btn btn-primary">Edit</router-link>
+          </td>
+          <td>
+            <button class="btn btn-danger" @click.prevent="deletePost(post._id, index)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+  created() {
+    let uri = "http://localhost:4000/posts";
+    this.axios.get(uri).then(response => {
+      this.posts = response.data;
+    });
+  },
+  methods: {
+    deletePost(id, idx) {
+      let uri = `http://localhost:4000/posts/delete/${id}`;
+      this.axios.delete(uri).then(response => {
+        this.posts.splice(idx, 1);
+      });
+    }
+  }
+};
+</script>
